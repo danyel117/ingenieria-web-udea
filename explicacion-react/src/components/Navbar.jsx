04821ac3e01ce241f3204db8c7de0fa22b42bade
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-12-15 12:45:44 +0000

    agregar explicacion de estados, efectos y formularios

--- explicacion-react/src/components/Navbar.jsx
+++ explicacion-react/src/components/Navbar.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../logo.svg';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const [showResponsiveNavbar, setShowResponsiveNavbar] = useState(false);
   return (
-    <nav className='flex items-center bg-indigo-700'>
+    <div>
+      <div className='p-2 md:hidden'>
+        <button
+          onClick={() => {
+            setShowResponsiveNavbar(!showResponsiveNavbar);
+          }}
+        >
+          <i className='fas fa-bars' />
+        </button>
+        {showResponsiveNavbar && (
+          <nav className='flex items-center bg-indigo-700'>
+            <ul className='flex flex-col p-3'>
+              <LinkNavegacion texto='Inicio' ruta='/' />
+              <LinkNavegacion texto='Perfil' ruta='/perfil' />
+              <LinkNavegacion texto='Contacto' ruta='/contacto' />
+            </ul>
+          </nav>
+        )}
+      </div>
+      <NavbarBig />
+    </div>
+  );
+};
+
+const NavbarBig = () => {
+  return (
+    <nav className='hidden md:flex items-center bg-indigo-700'>
       <img className='mx-2 h-16' src={logo} alt='Logo React' />
       <ul className='flex'>
         <LinkNavegacion texto='Inicio' ruta='/' />
         <LinkNavegacion texto='Perfil' ruta='/perfil' />
-        <LinkNavegacion texto='Contacto' ruta='/contacto' />
-        <LinkNavegacion texto='Contacto' ruta='/contacto' />
-        <LinkNavegacion texto='Contacto' ruta='/contacto' />
-        <LinkNavegacion texto='Contacto' ruta='/contacto' />
-        <LinkNavegacion texto='Contacto' ruta='/contacto' />
-        <LinkNavegacion texto='Contacto' ruta='/contacto' />
-        <LinkNavegacion texto='Contacto' ruta='/contacto' />
-        <LinkNavegacion texto='Contacto' ruta='/contacto' />
         <LinkNavegacion texto='Contacto' ruta='/contacto' />
       </ul>
     </nav>
